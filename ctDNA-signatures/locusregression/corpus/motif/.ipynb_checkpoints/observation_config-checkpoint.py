975804bc877ddafd7fd49c81acfb2576552cbe90
Ann Caplin <ann.caplin@gmail.com>
2024-03-11 13:14:23 -0400

    Make fragment ingestion plots

--- ctDNA-signatures/locusregression/corpus/motif/.ipynb_checkpoints/observation_config-checkpoint.py
+++ ctDNA-signatures/locusregression/corpus/motif/.ipynb_checkpoints/observation_config-checkpoint.py
@@ -24,13 +24,6 @@ CONTEXTS = sorted(
 
 CONTEXT_IDX = dict(zip(CONTEXTS, range(len(CONTEXTS))))
 
-CONTEXTS_REV = sorted(
-    map(lambda x : ''.join(x), product('ATCG','ATCG','ATCG', 'ATCG')), 
-    key = lambda x : (x[3], x[2], x[1], x[0])
-    )
-
-CONTEXT_IDX_REV = dict(zip(CONTEXTS_REV, range(len(CONTEXTS_REV))))
-
 MUTATIONS = {
     context : [alt for alt in 'ACGT' if not alt == context[1]] # nucleotide cannot mutate to itself
     for context in CONTEXTS
@@ -115,67 +108,6 @@ class MotifSample(Sample):
         ax.set(yticks = [0,extent], xticks = [], 
                xlim = (-1,len(CONTEXTS)), ylim = (-0.1, 1.1*extent))
         ax.set_xticks(range(len(CONTEXTS)), CONTEXTS)
-        ax.tick_params(axis='x', labelrotation=90)
-        ax.axhline(0, color = 'lightgrey', linewidth = 0.25)
-
-        for s in ['left','right','top','bottom']:
-            ax.spines[s].set_visible(False)
-
-        return ax
-
-
-
-
-    def plot_rev(self, ax=None, figsize=(30,3), show_strand=True,**kwargs):
-        #print(CONTEXTS)
-        context_dist = np.zeros((self.N_CONTEXTS,))
-        mutation_dist = np.zeros((self.N_CONTEXTS, self.N_MUTATIONS,))
-
-        for context_idx, mutation_idx, weight in zip(
-            self.context, self.mutation, self.weight
-        ):
-            context_dist[CONTEXT_IDX_REV[CONTEXTS[context_idx]]] += weight
-
-        return self.plot_factorized_rev(context_dist, [], None, 
-                                   ax=ax, 
-                                   figsize=figsize,
-                                   show_strand=show_strand, 
-                                   **kwargs)
-        
-
-    @staticmethod
-    def plot_factorized_rev(context_dist, mutation_dist, attribute_dist, 
-                        ax=None, figsize=(5,3), show_strand=True,**kwargs):
-
-        #joint_prob = (context_dist[:,None]*mutation_dist).ravel() # CxM
-        #event_name = [(to_cosmic_str(c,m),'f') if c[1] in 'TC' else (to_cosmic_str(revcomp(c), complement[m]), 'r')
-        #              for c in CONTEXTS for m in MUTATIONS[c]
-        #             ]
-        
-        #event_prob = dict(zip(event_name, joint_prob))
-
-        #fwd_events = np.array([event_prob[(event, 'f')] for event in COSMIC_SORT_ORDER])
-        #rev_events = np.array([event_prob[(event, 'r')] for event in COSMIC_SORT_ORDER])
-
-        
-        if ax is None:
-            _, ax = plt.subplots(1,1,figsize= figsize)
-
-        plot_kw = dict(
-            x = range(len(CONTEXTS)),
-            color = COLOR_LIST,
-            width = 1,
-            edgecolor = 'white',
-            linewidth = 0.5,
-            #error_kw = {'alpha' : 0.5, 'linewidth' : 0.5}
-        )
-        extent = max(context_dist)
-        #print(CONTEXTS_REV)
-        
-        ax.bar(height = context_dist, **plot_kw)
-        ax.set(yticks = [0,extent], xticks = [], 
-               xlim = (-1,len(CONTEXTS)), ylim = (-0.1, 1.1*extent))
-        ax.set_xticks(range(len(CONTEXTS)), CONTEXTS_REV)
         ax.tick_params(axis='x', labelrotation=90)
         ax.axhline(0, color = 'lightgrey', linewidth = 0.25)
 
